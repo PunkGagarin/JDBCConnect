c536aae054f228ab85fe0353ab36bbf7ff4c13c1
Roman <41580710+PunkGagarin@users.noreply.github.com>
2019-02-16 17:21:43 +0300

    add static Query import
    add static Query import

--- src/ru/andersen/gagarin/DataBaseActions.java
+++ src/ru/andersen/gagarin/DataBaseActions.java
@@ -4,20 +4,22 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static ru.andersen.gagarin.Query.*;
+
 public class DataBaseActions {
     static void backupDB(Connection c) throws SQLException {
-        execute(c,Query.BackupDB);
+        execute(c, BackupDB);
     }
 
     static void dropDB(Connection c) throws SQLException {
-        execute(c,Query.DropDB);
+        execute(c, DropDB);
     }
 
-    static void createDB(Connection c) throws SQLException{
-        execute(c,Query.CreateDB);
+    static void createDB(Connection c) throws SQLException {
+        execute(c, CreateDB);
     }
 
-    static void createTables(Connection c) throws SQLException{
+    static void createTables(Connection c) throws SQLException {
         useDB(c);
         createLocationTable(c);
         createStuffTable(c);
@@ -29,64 +31,65 @@ public class DataBaseActions {
         createDateTable(c);
     }
 
-    private static void useDB(Connection c) throws SQLException{
-        execute(c,Query.UseDB);
-    }
-    private static void createLocationTable(Connection c) throws SQLException{
-        execute(c,Query.LocationTable);
+    private static void useDB(Connection c) throws SQLException {
+        execute(c, UseDB);
     }
 
-    private static void createStuffTable(Connection c) throws SQLException{
-        execute(c,Query.StuffTable);
+    private static void createLocationTable(Connection c) throws SQLException {
+        execute(c, LocationTable);
     }
 
-    private static void createUserTypeTable(Connection c) throws SQLException{
-        execute(c,Query.UserTypeTable);
+    private static void createStuffTable(Connection c) throws SQLException {
+        execute(c, StuffTable);
     }
 
-    private static void createSkyCharTable(Connection c) throws SQLException{
-        execute(c,Query.SkyCharTable);
+    private static void createUserTypeTable(Connection c) throws SQLException {
+        execute(c, UserTypeTable);
     }
 
-    private static void createWindCharTable(Connection c) throws SQLException{
-        execute(c,Query.WindCharTable);
+    private static void createSkyCharTable(Connection c) throws SQLException {
+        execute(c, SkyCharTable);
     }
 
-    private static void createUserTable(Connection c) throws SQLException{
-        execute(c,Query.UserTable);
+    private static void createWindCharTable(Connection c) throws SQLException {
+        execute(c, WindCharTable);
     }
 
-    private static void createWeatherCharTable(Connection c) throws SQLException{
-        execute(c,Query.WeatherCharTable);
+    private static void createUserTable(Connection c) throws SQLException {
+        execute(c, UserTable);
     }
 
-    private static void createDateTable(Connection c) throws SQLException{
-        execute(c,Query.DateTable);
+    private static void createWeatherCharTable(Connection c) throws SQLException {
+        execute(c, WeatherCharTable);
+    }
+
+    private static void createDateTable(Connection c) throws SQLException {
+        execute(c, DateTable);
     }
 
 
-    static void createFK(Connection c) throws SQLException{
-        execute(c,Query.FK_TypeUser);
-        execute(c,Query.FK_LocationUser);
-        execute(c,Query.FK_WindWeather);
-        execute(c,Query.FK_SkyWeather);
-        execute(c,Query.FK_LocationDate);
-        execute(c,Query.FK_WeatherDate);
-        execute(c,Query.FK_StuffDate);
+    static void createFK(Connection c) throws SQLException {
+        execute(c, FK_TypeUser);
+        execute(c, FK_LocationUser);
+        execute(c, FK_WindWeather);
+        execute(c, FK_SkyWeather);
+        execute(c, FK_LocationDate);
+        execute(c, FK_WeatherDate);
+        execute(c, FK_StuffDate);
     }
 
-    static void insertInfo(Connection c) throws SQLException{
-        execute(c,Query.InsertIntoWind);
-        execute(c,Query.InsertIntoWind2);
-        execute(c,Query.InsertIntoWeather);
+    static void insertInfo(Connection c) throws SQLException {
+        execute(c, InsertIntoWind);
+        execute(c, InsertIntoWind2);
+        execute(c, InsertIntoWeather);
 
     }
 
-    static ResultSet getResultAction(Connection c) throws SQLException{
+    static ResultSet getResultAction(Connection c) throws SQLException {
         return c.prepareStatement("SELECT * FROM weatherdb.windchar;").executeQuery();
     }
 
-    private static void execute(Connection c, Query q) throws SQLException{
+    private static void execute(Connection c, Query q) throws SQLException {
         c.prepareStatement(q.getQuery()).execute();
     }
 
